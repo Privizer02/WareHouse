e9798d3fa8a9a751aba46bca191c86394dca0dfc
Privizer02 <milanprivizer2002@gmail.com>
2024-01-12 16:43:50 +0100

    komponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './core/nav/nav.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule,
